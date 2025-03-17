39275bc972baf8ec950fae2794fea20bd9097140
winchesterrx <123671820+winchesterrx@users.noreply.github.com>
2025-03-17 09:08:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
@@ -21,8 +22,10 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics /> {/* Adicionando Vercel Analytics aqui */}
     </TooltipProvider>
   </QueryClientProvider>
 );
 
 export default App;
+
